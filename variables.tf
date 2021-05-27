f0a2445f7b2b4d67c93689263dddb49bae31a99d
SBKGit <52453070+SBKGit@users.noreply.github.com>
2021-05-27 22:53:37 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,17 @@ variable "ami_version" {
   type        = string
   default     = "ami-0b2d4c8a29d3cff80"
 }
+
+variable "vpc_cidr" {
+	default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = "list"
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["us-east-2", "us-west-2"]
+}
