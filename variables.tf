5fe4bdb1dea27a94af578b7bf1060b5fbb840f44
SBKGit <52453070+SBKGit@users.noreply.github.com>
2021-05-27 12:08:33 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,18 @@
 variable "region" {
   description = "AWS Region"
-  default     = "eu-west-2"
+  access_key  = "AKIAY5NLBQSSGZGXF7SA"
+  secret_key  = "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"
+  region      = "eu-east-1"
 }  
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_version" {
+  description = "Version of the AMI to deploy"
+  type        = string
+  default     = "ami-0b2d4c8a29d3cff80"
+}
