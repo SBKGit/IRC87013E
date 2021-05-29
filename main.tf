7a62d849da57425fcc17b4c10a13603735150540
SBKGit <shireesh.kantharaj@gmail.com>
2021-05-29 23:43:32 +0100

    Initial working tf apply

--- main.tf
+++ main.tf
@@ -1,128 +1,48 @@
-resource "aws_instance" "base"{
-  ami                    = var.ami_version
-  instance_type          = var.instance_type
-  count                  = var.no-of-instances
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.terraform-sbk-elb.id]
-  user_data              = "${file("install_httpd.sh")}"
 
-  tags ={
-    Name = "sbktest${count.index}"
-  }
-}
-
-resource "aws_eip" "myeip"{
-  count =length(aws_instance.base)
-  vpc = true
-  instance = "${element(aws_instance.base.*.id,count.index)}"
-
-  tags = {
-    Name ="eip-sbk${count.index + 1}" 
-  }
-}
-
-
-## Create VPC ##
-resource "aws_vpc" "terraform-vpc" {
-  cidr_block       = "172.16.0.0/16"
+# VPC
+resource "aws_vpc" "terra_vpc" {
+  cidr_block       = "${var.vpc_cidr}"
   enable_dns_hostnames = true
-  tags = {
-    Name = "terraform-demo-vpc"
+  tags =  {
+    Name = "Terraform_VPC"
   }
 }
 
-resource "aws_subnet" "terraform-subnet_1" {
-  vpc_id     = "${aws_vpc.terraform-vpc.id}"
-  cidr_block = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
+ resource "aws_subnet" "subnet1" {
+   count = "${length(var.subnets_cidr2)}"
+   vpc_id     = "${aws_vpc.terra_vpc.id}"
+   cidr_block = "${element(var.subnets_cidr2,count.index)}"
+   availability_zone = var.availability_zone1[0]
 
-  tags = {
-    Name = "terraform-subnet_1"
-  }
-}
 
-resource "aws_lb_target_group" "my-target-group" {
-  health_check {
-    interval            = 10
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-  }
+  tags  =  {
+    Name = "app-subnet-1"
+    }
+ }
 
-  name        = "my-test-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = "${aws_vpc.terraform-vpc.id}"
-}
-
-## Security Group for ELB
-resource "aws_security_group" "elb" {
-  name = "terraform-sbk-elb"
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-### Creating ELB
-  
-resource "aws_elb" "sbk" {
-  name = "terraform-asg-sbk"
-  security_groups = ["${aws_security_group.elb.id}"]
-  subnets = data.aws_subnet_ids.subnet.ids
-  #availability_zones = ["${data.aws_availability_zones.all.names}"]
-  health_check {
-    healthy_threshold = 10
-    unhealthy_threshold = 10
-    timeout = 3
-    interval = 30
-    target = "HTTP:80/"
-  }
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "80"
-    instance_protocol = "http"
-  }
-}
-
-resource "aws_alb_target_group_attachment" "ec2_attach" {
-  count = length(aws_instance.base)
-  target_group_arn = aws_lb_target_group.my-target-group.arn
-  target_id = aws_instance.base[count.index].id
-}
-  
 # Internet Gateway
 resource "aws_internet_gateway" "terra_igw" {
-  vpc_id = "${aws_vpc.terraform-vpc.id}"
+  vpc_id = "${aws_vpc.terra_vpc.id}"
   tags =  {
     Name = "main"
   }
 }
 
-# # Subnets : public
+# Subnets : public
 resource "aws_subnet" "public" {
-  count   = "${length(var.subnets_cidr)}"
-  vpc_id  = "${aws_vpc.terraform-vpc.id}"
+  count = "${length(var.subnets_cidr)}"
+  vpc_id = "${aws_vpc.terra_vpc.id}"
   cidr_block = "${element(var.subnets_cidr,count.index)}"
-  availability_zone = "us-east-1a"
+  availability_zone = "${element(var.azs,count.index)}"
+  map_public_ip_on_launch = true
   tags =  {
     Name = "Subnet-${count.index+1}"
   }
 }
 
-# # Route table: attach Internet Gateway 
+# Route table: attach Internet Gateway 
 resource "aws_route_table" "public_rt" {
-  vpc_id = "${aws_vpc.terraform-vpc.id}"
+  vpc_id = "${aws_vpc.terra_vpc.id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.terra_igw.id}"
@@ -132,9 +52,55 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# # Route table association with public subnets
+# Route table association with public subnets
 resource "aws_route_table_association" "a" {
   count = "${length(var.subnets_cidr)}"
   subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
   route_table_id = "${aws_route_table.public_rt.id}"
+}
+resource "aws_route_table_association" "public-assoc-1" {
+  count = "${length(var.subnets_cidr)}"
+  subnet_id      = "${aws_subnet.subnet1[count.index].id}"
+  route_table_id = "${aws_route_table.public_rt.id}"
+}
+
+resource "aws_security_group" "webservers" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = "${aws_vpc.terra_vpc.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "base"{
+  ami                    = var.ami_version
+  instance_type          = var.instance_type
+  count                  = var.no-of-instances
+  key_name               = var.key_name
+  # security_groups        = ["${aws_security_group.webservers.id}"]
+  vpc_security_group_ids = ["${aws_security_group.webservers.id}"]
+  subnet_id              = aws_subnet.subnet1[count.index].id
+  user_data              = "${file("install_httpd.sh")}"
+  tags ={
+    Name = "sbktest${count.index}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,42 @@
-variable "region" {
+variable "aws_region" {
   description = "AWS Region"
   default     = "us-east-1"
 }  
 
+variable "key_name" {
+  description = "Key name for SSHing into EC2"
+  default = "sbktest"
+}
+
+variable "vpc_cidr" {
+	default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = "list"
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "subnets_cidr2" {
+	type = "list"
+	default = ["10.20.3.0/24", "10.20.4.0/24"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "amiid" {
+  default = "ami-04b2519c83e2a7ea5"
+}
+
 variable "instance_type" {
-  description = "The type of EC2 Instances to run"
-  type        = string
-  default     = "t2.micro"
+  default = "t2.micro"
+}
+
+variable "no-of-instances" {
+  default = "2"
 }
 
 variable "ami_version" {
@@ -15,25 +45,6 @@ variable "ami_version" {
   default     = "ami-0b2d4c8a29d3cff80"
 }
 
-variable "no-of-instances" {
-  default = 2
-}
-
-variable "key_name" {
-  description = "Key name for SSHing into EC2"
-  default = "sbktest"
-}
-
-variable "vpc_cidr" {
-  default = "10.20.0.0/16"
-}
-
-variable "subnets_cidr" {
-  type = "list"
-  default = ["10.20.1.0/24", "10.20.2.0/24"]
-}
-
-variable "azs" {
-  type = "list"
+variable "availability_zone1" {
   default = ["us-east-1a", "us-east-1b"]
 }
