c3f348b9eaad089132d53b15ba3060903a798057
Shireesh Kantharaj <52453070+SBKGit@users.noreply.github.com>
2023-01-12 11:08:45 +0000

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 sudo apt-get -y update
 sudo service nginx start
-echo "<h1>Hello World</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Hello World</h1>" | sudo tee /var/www/html/index.ht
